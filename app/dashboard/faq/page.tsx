b167b84024bcbd60b14b480e0e5c24905961f82c
Nitin S <28nitin07@gmail.com>
2025-12-17 20:45:03 +0530

    fix: build errors

--- app/dashboard/faq/page.tsx
+++ app/dashboard/faq/page.tsx
@@ -3,41 +3,45 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { ReactNode } from "react";
 
+interface MarkdownComponentProps {
+  children?: ReactNode;
+}
 
 const MarkdownComponents = {
  
-  h3: ({ children }: any) => (
+  h3: ({ children }: MarkdownComponentProps) => (
     <h3 className="text-lg font-bold my-4">
       {children}
     </h3>
   ),
 
-  h2: ({ children }: any) => (
+  h2: ({ children }: MarkdownComponentProps) => (
     <h2 className="text-xl font-bold my-6 text-center">
       {children}
     </h2>
   ),
 
-  h1: ({ children }: any) => (
+  h1: ({ children }: MarkdownComponentProps) => (
     <h1 className="text-2xl font-bold my-8 text-center">
       {children}
     </h1>
   ),
 
-  p: ({ children }: any) => (
+  p: ({ children }: MarkdownComponentProps) => (
     <p className="my-2 text-gray-800">
       {children}
     </p>
   ),
 
-  ul: ({ children }: any) => (
+  ul: ({ children }: MarkdownComponentProps) => (
     <ul className="list-disc list-inside space-y-2 my-4 ml-4">
       {children}
     </ul>
   ),
 
-  ol: ({ children }: any) => (
+  ol: ({ children }: MarkdownComponentProps) => (
     <ol className="list-decimal list-inside space-y-2 my-4 ml-4">
       {children}
     </ol>
